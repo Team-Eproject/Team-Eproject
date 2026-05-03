1227fec7623035e4cc49e2c3d05cb9809509ef00
MIT-RareTECH <mitsuki.swimmer@gmail.com>
2026-05-04 00:44:11 +0900

    ユーザー新規登録

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,5 +1,20 @@
 from django.urls import path, include
+from django.views.generic immport CreateView
+from django.views.generic import CreateView
+from .views import HomeView
 
 urlpatterns = [
     path("api/users/", include("apps.users.urls")),
+
+#新規登録ページの作成
+    path('signup/', CreateView.as_view(
+        template_name = '',
+        form_class=UserCreationForm,
+        success_url='/foods',
+    ), name='signup'),
+
+    path('home/' HomeView.as_view(
+        template_name='',
+    ),
+     name='home'),
 ]
