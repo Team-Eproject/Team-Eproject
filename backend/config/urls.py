79586bb4e94b77fce361b1836f9f4972c4df75e1
Taka51期 <sh.takanori@gmail.com>
2026-05-14 15:06:54 +0900

    エラー多数出てたため修正

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,17 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-<<<<<<< HEAD
 from django.views.generic import CreateView
 from django.contrib.auth.forms import UserCreationForm
-from .views import HomeView
-=======
+# from .views import HomeView
 # HomeViewでエラーが出てたのでいったん外しました。
->>>>>>> 18e27a8a3443da8a9e46d01aad93fb44879ce0b2
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-
-<<<<<<< HEAD
+    path("", include("web.urls")),
     path("api/", include("apps.users.urls")),
     path("api/", include("apps.foods.urls")),
     path("api/users/", include("apps.users.urls")),
@@ -23,18 +19,13 @@ urlpatterns = [
         success_url='/foods',
     ), name='signup'),
 
-    path('home/', HomeView.as_view(
-        template_name='',
-    ),
-     name='home'),
+    # path('home/', HomeView.as_view(
+    #     template_name='',
+    # ),
+    #  name='home'),
 
-]
-=======
-    # 画面表示
-    path("", include("web.urls")),
 
     # API
     path("api/users/", include("apps.users.urls")),
     path("api/foods/", include("apps.foods.urls")),
 ]
->>>>>>> 18e27a8a3443da8a9e46d01aad93fb44879ce0b2
