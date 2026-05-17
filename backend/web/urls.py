1acf5403f635d5cd17ffdd60d2adbd43aa07b5b3
kakano <kariblue320@gmail.com>
2026-05-17 21:30:32 +0900

    DRF仕様対応その1 signup.htmlとlogin.htmlに記述追記

--- backend/web/urls.py
+++ backend/web/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.top, name="top"),
-    path("signup/", views.signup, name="signup"),
-    path("login/", views.login, name="login"),
+    path("signup/", views.signup_view, name="signup"),
+    path("login/", views.login_view, name="login"),
     path("main/", views.main, name="main"),
 ]
