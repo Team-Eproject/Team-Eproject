63615a7a23aba0727c354293707ac5324f9e50c5
MIT-RareTECH <mitsuki.swimmer@gmail.com>
2026-05-25 21:09:05 +0900

    AIレシピ編集

--- backend/apps/foods/urls.py
+++ backend/apps/foods/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
-from .views import FoodListView, FoodCreateView, CategoryListView, FoodCreateAPIView, register_page
+from .views import(
+    FoodListView, 
+    FoodCreateView, 
+    CategoryListView, 
+    FoodCreateAPIView, 
+    register_page,
+    ai_menu_process,
+)    
 
 
 
@@ -9,5 +16,5 @@ urlpatterns =  [
     path("foods/categories/", CategoryListView.as_view()),
     path("foods/entry/", FoodCreateView.as_view(), name="food-entry"),
     path("register/", register_page),
-    path("recipe/", views.ai_menu_process, name="ai_menu_process"),
+    path("recipe/", ai_menu_process, name="ai_menu_process"),
 ]
